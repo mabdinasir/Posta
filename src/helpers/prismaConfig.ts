feb655b354086027543535fa5e3176beb6328fcb
Mohamed Abdinasir <abdelnacerr@gmail.com>
2023-05-19 16:59:50 +1000

    revert prisma config

--- src/helpers/prismaConfig.ts
+++ src/helpers/prismaConfig.ts
@@ -1,8 +1,7 @@
-import { PrismaClient } from '../deps.ts'
-import { getEnv } from './envLoader.ts'
+import "https://deno.land/std@0.173.0/dotenv/load.ts";
+import { PrismaClient } from "../deps.ts";
 
-await getEnv()
-const PRISMA_CLIENT_URL = Deno.env.get('PRISMA_CLIENT_URL') as string
+const PRISMA_CLIENT_URL = Deno.env.get("PRISMA_CLIENT_URL") as string;
 
 const prisma = new PrismaClient({
 	datasources: {
@@ -10,6 +9,6 @@ const prisma = new PrismaClient({
 			url: PRISMA_CLIENT_URL,
 		},
 	},
-})
+});
 
-export { prisma }
+export { prisma };
